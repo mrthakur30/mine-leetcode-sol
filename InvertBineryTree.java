a2275c17ba32da5391b2af9ebf71ecca1a196d9e
Mukul Thakur <mukulxt@gmail.com>
2023-01-24 18:53:21 +0530

    Invert Binary Tree

---
+++ InvertBineryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public void swap(TreeNode root){
+        if(root==null) return ;
+        TreeNode temp = root.left ;
+        root.left = root.right ;
+        root.right = temp ;
+        swap(root.left);
+        swap(root.right);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        swap(root);
+        return root;
+    }
+
+}
